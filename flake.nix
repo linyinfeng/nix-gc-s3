677e75daa0f27a275a079d62afc3ce77cbb22f67
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-04-20 21:38:06 +0800

    Add flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus";
+  };
+
+  outputs = { self, nixpkgs, flake-utils-plus }@inputs:
+    let
+      utils = flake-utils-plus.lib;
+      inherit (nixpkgs) lib;
+    in
+    utils.mkFlake
+      {
+        inherit self inputs;
+
+        channelsConfig = {
+          allowAliases = false;
+        };
+
+        outputsBuilder = channels:
+          let
+            pkgs = channels.nixpkgs;
+          in
+          rec {
+            packages.nix-gc-s3 = pkgs.callPackage ./nix-gc-s3.nix {};
+            devShells.default = pkgs.callPackage ./shell.nix {};
+            checks = packages;
+          };
+      };
+}
