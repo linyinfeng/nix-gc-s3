37cd455d3022212420fd16db2ed66385d5f7ac8b
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-09-13 00:33:18 +0800

    Cleanup inputs

--- flake.nix
+++ flake.nix
@@ -11,7 +11,6 @@
     poetry2nix.inputs.treefmt-nix.follows = "treefmt-nix";
     devshell.url = "github:numtide/devshell";
     devshell.inputs.nixpkgs.follows = "nixpkgs";
-    devshell.inputs.flake-utils.follows = "flake-utils";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
